7070af809f3e6c7f1ff4ded13b6bb4641e110e02
Danilo Pedrosa <dmop@cin.ufpe.br>
2016-09-21 11:06:53 -0300

    Primeiro commit
    Identidade  visual basica

---
+++ app.js
@@ -0,0 +1,22 @@
+/*var app = angular.module('plunker', []);
+
+app.controller('MainCtrl', function($scope) {
+  $scope.name = 'World';
+});
+*/
+
+var myApp = angular.module('social', []);
+
+
+    myApp.controller('FeedController', ['$scope','$http', function($scope, $http){
+        $scope.title = 'Seu Feed';
+
+
+        $http.get('posts.json').success(function(data){
+
+            $scope.lista = data;
+
+
+        });
+
+    }]);
